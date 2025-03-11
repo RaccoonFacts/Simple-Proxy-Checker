88c0cf48fde9530c30262351c8f2e02e5daac441
Luck <Luck@DEATH-TO-JEWS>
2025-03-12 06:11:11 +0900

    Add project files.

---
+++ ProxyChecker/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ProxyChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.totalProxyList = new System.Windows.Forms.TextBox();
+            this.badProxyList = new System.Windows.Forms.TextBox();
+            this.workingProxyList = new System.Windows.Forms.TextBox();
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.TotalListLbl = new System.Windows.Forms.Label();
+            this.brokenLbl = new System.Windows.Forms.Label();
+            this.workingLst = new System.Windows.Forms.Label();
+            this.TimeRemainingLbl = new System.Windows.Forms.Label();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // totalProxyList
+            // 
+            this.totalProxyList.Location = new System.Drawing.Point(12, 34);
+            this.totalProxyList.Multiline = true;
+            this.totalProxyList.Name = "totalProxyList";
+            this.totalProxyList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.totalProxyList.Size = new System.Drawing.Size(184, 326);
+            this.totalProxyList.TabIndex = 0;
+            // 
+            // badProxyList
+            // 
+            this.badProxyList.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.badProxyList.Location = new System.Drawing.Point(202, 34);
+            this.badProxyList.Multiline = true;
+            this.badProxyList.Name = "badProxyList";
+            this.badProxyList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.badProxyList.Size = new System.Drawing.Size(184, 326);
+            this.badProxyList.TabIndex = 1;
+            // 
+            // workingProxyList
+            // 
+            this.workingProxyList.Location = new System.Drawing.Point(392, 34);
+            this.workingProxyList.Multiline = true;
+            this.workingProxyList.Name = "workingProxyList";
+            this.workingProxyList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.workingProxyList.Size = new System.Drawing.Size(184, 326);
+            this.workingProxyList.TabIndex = 2;
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Font = new System.Drawing.Font("OCR A Extended", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBtn.Location = new System.Drawing.Point(14, 366);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(183, 37);
+            this.checkBtn.TabIndex = 3;
+            this.checkBtn.Text = "Check";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            this.checkBtn.Click += new System.EventHandler(this.checkBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 433);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(564, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // TotalListLbl
+            // 
+            this.TotalListLbl.AutoSize = true;
+            this.TotalListLbl.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalListLbl.Location = new System.Drawing.Point(12, 19);
+            this.TotalListLbl.Name = "TotalListLbl";
+            this.TotalListLbl.Size = new System.Drawing.Size(159, 12);
+            this.TotalListLbl.TabIndex = 5;
+            this.TotalListLbl.Text = "Paste Proxy List Here:";
+            // 
+            // brokenLbl
+            // 
+            this.brokenLbl.AutoSize = true;
+            this.brokenLbl.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brokenLbl.Location = new System.Drawing.Point(200, 19);
+            this.brokenLbl.Name = "brokenLbl";
+            this.brokenLbl.Size = new System.Drawing.Size(110, 12);
+            this.brokenLbl.TabIndex = 6;
+            this.brokenLbl.Text = "Broken Proxies:";
+            // 
+            // workingLst
+            // 
+            this.workingLst.AutoSize = true;
+            this.workingLst.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workingLst.Location = new System.Drawing.Point(390, 19);
+            this.workingLst.Name = "workingLst";
+            this.workingLst.Size = new System.Drawing.Size(117, 12);
+            this.workingLst.TabIndex = 7;
+            this.workingLst.Text = "Working Proxies:";
+            // 
+            // TimeRemainingLbl
+            // 
+            this.TimeRemainingLbl.AutoSize = true;
+            this.TimeRemainingLbl.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeRemainingLbl.Location = new System.Drawing.Point(10, 418);
+            this.TimeRemainingLbl.Name = "TimeRemainingLbl";
+            this.TimeRemainingLbl.Size = new System.Drawing.Size(117, 12);
+            this.TimeRemainingLbl.TabIndex = 8;
+            this.TimeRemainingLbl.Text = "Time Remaining: ";
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Font = new System.Drawing.Font("OCR A Extended", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyBtn.Location = new System.Drawing.Point(393, 366);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(183, 37);
+            this.copyBtn.TabIndex = 9;
+            this.copyBtn.Text = "Copy Above";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 470);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.TimeRemainingLbl);
+            this.Controls.Add(this.workingLst);
+            this.Controls.Add(this.brokenLbl);
+            this.Controls.Add(this.TotalListLbl);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBtn);
+            this.Controls.Add(this.workingProxyList);
+            this.Controls.Add(this.badProxyList);
+            this.Controls.Add(this.totalProxyList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simple Proxy Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox totalProxyList;
+        private System.Windows.Forms.TextBox badProxyList;
+        private System.Windows.Forms.TextBox workingProxyList;
+        private System.Windows.Forms.Button checkBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label TotalListLbl;
+        private System.Windows.Forms.Label brokenLbl;
+        private System.Windows.Forms.Label workingLst;
+        private System.Windows.Forms.Label TimeRemainingLbl;
+        private System.Windows.Forms.Button copyBtn;
+    }
+}
+
---
+++ ProxyChecker/Form1.cs
@@ -0,0 +1,150 @@
+﻿// ProxyChecker v1.0
+// This application checks the validity of proxy servers by testing their connectivity.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProxyChecker
+{
+    public partial class Form1 : Form
+    {
+        // Placeholder text for the proxy list TextBox
+        private string proxyListPlaceHldr = "8.211.200.183:8080";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set placeholder text for the proxy list TextBox
+            totalProxyList.Text = proxyListPlaceHldr;
+            totalProxyList.ForeColor = Color.Gray;
+
+            // Attach event handlers to manage placeholder behavior
+            totalProxyList.GotFocus += RemovePlaceholder;
+            totalProxyList.LostFocus += SetPlaceholder;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form load event - currently not used but can be used for initialization
+        }
+
+        private async void checkBtn_Click(object sender, EventArgs e)
+        {
+            // Clear previous results before starting a new check
+            workingProxyList.Text = "";
+            badProxyList.Text = "";
+            progressBar1.Value = 0;
+
+            // Disable the button while checking proxies to prevent multiple clicks
+            checkBtn.Enabled = false;
+
+            // Split the input proxy list into an array of proxies
+            string[] proxies = totalProxyList.Text.Split(new[] { '\n', '\r' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // Set progress bar properties
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = proxies.Length;
+
+            int currentProgress = 0;
+            foreach (string proxy in proxies)
+            {
+                if (string.IsNullOrWhiteSpace(proxy)) continue; // Skip empty lines
+
+                // Check if the proxy is working
+                bool isWorking = await TestProxyAsync(proxy.Trim());
+
+                if (isWorking)
+                {
+                    workingProxyList.AppendText(proxy + Environment.NewLine); // Add to working list
+                }
+                else
+                {
+                    badProxyList.AppendText(proxy + Environment.NewLine); // Add to bad list
+                }
+
+                // Update progress bar
+                currentProgress++;
+                progressBar1.Value = currentProgress;
+            }
+
+            // Re-enable the button after completion
+            checkBtn.Enabled = true;
+
+            MessageBox.Show("Proxy Checking Complete");
+        }
+
+        private async Task<bool> TestProxyAsync(string proxy)
+        {
+            try
+            {
+                // Split the proxy into IP and port
+                string[] parts = proxy.Split(':');
+                if (parts.Length != 2) return false;
+
+                string ip = parts[0];
+                int port = int.Parse(parts[1]);
+
+                // Configure HttpClient with proxy
+                var handler = new HttpClientHandler
+                {
+                    Proxy = new System.Net.WebProxy(ip, port),
+                    UseProxy = true,
+                };
+
+                using (var client = new HttpClient(handler))
+                {
+                    // Set timeout to 5 seconds to avoid long delays
+                    client.Timeout = TimeSpan.FromSeconds(5);
+
+                    // Attempt to make a GET request to Google
+                    var response = await client.GetAsync("http://www.google.com");
+                    return response.IsSuccessStatusCode; // Returns true if the request succeeds
+                }
+            }
+            catch (Exception)
+            {
+                return false; // Return false if an error occurs
+            }
+        }
+
+        private void RemovePlaceholder(object sender, EventArgs e)
+        {
+            // Remove placeholder text when the user focuses on the TextBox
+            if (totalProxyList.Text == proxyListPlaceHldr)
+            {
+                totalProxyList.Text = "";
+                totalProxyList.ForeColor = Color.Black;
+            }
+        }
+
+        private void SetPlaceholder(object sender, EventArgs e)
+        {
+            // Restore placeholder text if the TextBox is empty when focus is lost
+            if (string.IsNullOrWhiteSpace(totalProxyList.Text))
+            {
+                totalProxyList.Text = proxyListPlaceHldr;
+                totalProxyList.ForeColor = Color.Gray;
+            }
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            // Copy working proxies to clipboard
+            if (!string.IsNullOrEmpty(workingProxyList.Text))
+            {
+                Clipboard.SetText(workingProxyList.Text);
+            }
+        }
+    }
+}
